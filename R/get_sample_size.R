3a44f01cadd26f086da35a67a8d10cbcf95898c2
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-03-01 16:43:15 -0500

    Add get_sample_size from Mungesumstats

---
+++ R/get_sample_size.R
@@ -0,0 +1,23 @@
+#' Get sample size
+#' 
+#' Infer sample size from summary stats using \pkg{MungeSumstats}.
+#' @inheritParams filter_snps
+#' @inheritParams MungeSumstats:::compute_sample_size
+#' @family general
+#' @export
+#' @examples
+#' dat <- echodata::BST1
+#' dat2 <- echodata::get_sample_size(dat = dat)
+get_sample_size <- function(dat,
+                            method = c("ldsc", "giant", "metal", "sum"),
+                            force_new = FALSE,
+                            verbose=TRUE){
+  d <- MungeSumstats:::standardise_sumstats_column_headers_crossplatform(dat)
+  message("--")
+  dat2 <- MungeSumstats:::compute_sample_size(sumstats_dt = d$sumstats_dt,
+                                              method = method, 
+                                              force_new = force_new) 
+  message("--")
+  dat2 <- MUNGESUMSTATS.to_echolocatoR(dat2)
+  return(dat2)
+}
--- R/reassign_lead_snps.R
+++ R/reassign_lead_snps.R
@@ -9,6 +9,7 @@
 #' @param grouping_vars Column names to group by when assigning lead SNPs.
 #' @param nThread Number of threads to parallelise across.
 #' @param verbose Print messages.
+#' 
 #' @export
 #' @importFrom dplyr %>%  slice_min n_distinct all_of group_by_at
 #' @importFrom data.table rbindlist
@@ -24,7 +25,6 @@ reassign_lead_snps <- function(merged_dat,
                                grouping_vars = c("Dataset", "Locus"),
                                nThread = 1,
                                verbose = TRUE) {  
-    P <- Effect <- NULL;
     messager("Reassigning lead SNPs by:",
              paste(grouping_vars,collapse = ", "), v=verbose)
     grouping_vars <- grouping_vars[grouping_vars %in% colnames(merged_dat)]
@@ -44,10 +44,7 @@ reassign_lead_snps <- function(merged_dat,
     #### Find lead SNPs ####
     lead_snps <- (merged_dat %>% 
         dplyr::group_by_at(.vars = dplyr::all_of(grouping_vars)) %>%
-        dplyr::slice_min(order_by = dplyr::vars(
-            c(pval_col, dplyr::desc(effect_col))), 
-            n = 1, 
-            with_ties = FALSE) %>%
+        dplyr::slice_min(order_by = dplyr::all_of(P)) %>%
         dplyr::mutate(leadSNP=TRUE)
         )
     #### Merge with original data ####
