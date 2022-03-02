2c444984dc0457d51268956c3f94f0cba0d28233
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-03-02 01:32:10 -0500

    Skip get_sample_size when N present

--- R/get_sample_size.R
+++ R/get_sample_size.R
@@ -3,7 +3,8 @@
 #' Infer sample size from summary stats using \pkg{MungeSumstats}.
 #' @inheritParams filter_snps
 #' @inheritParams MungeSumstats:::compute_sample_size
-#' @family general
+#' @importFrom stats setNames
+#' @importFrom data.table setnames
 #' @export
 #' @examples
 #' dat <- echodata::BST1
@@ -13,10 +14,17 @@ get_sample_size <- function(dat,
                             force_new = FALSE,
                             verbose=TRUE){
   requireNamespace("MungeSumstats")
-  if(is.null(method)) method <- "ldsc"
-  d <- MungeSumstats:::standardise_sumstats_column_headers_crossplatform(dat)
+  if("N" %in% colnames(dat) && force_new==FALSE) return(dat)
+  if(is.null(method)) method <- "ldsc" 
   message("--")
-  dat2 <- MungeSumstats:::compute_sample_size(sumstats_dt = d$sumstats_dt,
+  dat2 <- data.table::copy(dat)
+  if("N_controls" %in% colnames(dat2)){
+    data.table::setnames(dat2,"N_controls","N_CON")
+  }
+  if("N_cases" %in% colnames(dat2)){
+    data.table::setnames(dat2,"N_cases","N_CAS")
+  }
+  dat2 <- MungeSumstats:::compute_sample_size(sumstats_dt = dat2,
                                               method = method, 
                                               force_new = force_new) 
   #### Rename Neff to N ####
@@ -24,8 +32,8 @@ get_sample_size <- function(dat,
     if("Neff" %in% colnames(dat2)){
       data.table::setnames(dat2,"Neff","N")
     } 
-  }
+  } 
   message("--")
-  dat2 <- MUNGESUMSTATS.to_echolocatoR(dat2)
+  dat2 <- MUNGESUMSTATS.to_echolocatoR(dat2)  
   return(dat2)
 }
