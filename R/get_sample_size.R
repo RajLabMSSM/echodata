88e460796eb55694a826840bb848511cfddac0f3
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-03-15 19:18:10 -0400

    Update get_sample_size with new exported MungeSumstats func

--- R/get_sample_size.R
+++ R/get_sample_size.R
@@ -6,7 +6,7 @@
 #' @param ... Additional argument passed to \code{return_only} function, 
 #' if \code{return_only}  is not \code{NULL}. 
 #' @inheritParams filter_snps
-#' @inheritParams MungeSumstats:::compute_sample_size
+#' @inheritParams MungeSumstats::compute_nsize
 #' @importFrom stats setNames
 #' @importFrom data.table setnames
 #' @export
@@ -14,15 +14,21 @@
 #' dat <- echodata::BST1
 #' dat2 <- echodata::get_sample_size(dat = dat)
 get_sample_size <- function(dat,
-                            method = c("ldsc", "giant", "metal", "sum"),
+                            compute_n = c("ldsc", "giant", "metal", "sum"),
                             return_only=NULL,
                             force_new = FALSE,
                             verbose=TRUE,
                             ...){
   
-  if("N" %in% colnames(dat) && force_new==FALSE) return(dat)
   requireNamespace("MungeSumstats")
-  if(is.null(method)) method <- "ldsc" 
+  
+  if("N" %in% colnames(dat) && force_new==FALSE) return(dat)
+  #### Check method ### 
+  if(is.null(compute_n)){
+    compute_n <- "ldsc" 
+  } else {
+    compute_n <- tolower(compute_n)[1]
+  }
   message("--")
   dat2 <- data.table::copy(dat)
   if("N_controls" %in% colnames(dat2)){
@@ -31,9 +37,10 @@ get_sample_size <- function(dat,
   if("N_cases" %in% colnames(dat2)){
     data.table::setnames(dat2,"N_cases","N_CAS")
   }
-  dat2 <- MungeSumstats:::compute_sample_size(sumstats_dt = dat2,
-                                              method = method, 
-                                              force_new = force_new) 
+  dat2 <- MungeSumstats::compute_nsize(sumstats_dt = dat2,
+                                       compute_n = compute_n, 
+                                       return_list = FALSE,
+                                       force_new = force_new) 
   #### Rename Neff to N ####
   if(!"N" %in% colnames(dat2) || isTRUE(force_new)){
     if("Neff" %in% colnames(dat2)){
