dbf91c121397d0d1c600e9a66442d9536c211f60
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-03-17 22:52:29 +0000

    Enable preview to take df [skip ci]

--- R/preview.R
+++ R/preview.R
@@ -7,12 +7,18 @@
 #' @inheritParams data.table::fread
 #' @export
 #' @importFrom utils capture.output
+#' @importFrom methods is
 preview <- function(file,
                     nrows = 5L,
                     nThread = 1L) {  
-    header <-  get_header(file = file, 
-                          colnames_only = FALSE,
-                          nrows = nrows, 
-                          nThread = nThread) 
+    if(methods::is(file,"data.frame")){
+        header <- head(file, n = nrows)
+    } else {
+        header <- get_header(file = file, 
+                             colnames_only = FALSE,
+                             nrows = nrows, 
+                             nThread = nThread) 
+    }
+    #### Print table nicely####
     message(paste0(capture.output(header), collapse = "\n"))
 }
