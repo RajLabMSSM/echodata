47688ce3a1cced279773e4d98c786107f291b58a
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-03-23 02:23:59 +0000

    Push before sharing BD vignette

--- R/mungesumstats_to_echolocatoR.R
+++ R/mungesumstats_to_echolocatoR.R
@@ -1,22 +1,52 @@
 #' Convert from \pkg{MungeSumstats} to \pkg{echolocatoR} format
 #' 
 #' Convert column names between package-specific formats.
-#' @param dat Data
-#' @examples  
-#' dat <- dplyr::rename(echodata::BST1, BP=POS, FRQ=Freq, BETA=Effect)
-#' dat_echoR <- mungesumstats_to_echolocatoR(dat=dat) 
+#' Optionally, can also be used to first automatically 
+#' standardise column names by 
+#' setting \code{standardise_colnames=TRUE} before converting to 
+#' \pkg{echolocatoR} format.
+#' 
+#' @param dat A \link[data.table]{data.table} of SNP-level summary statistics. 
+#' @param standardise_colnames Automatically rename all columns to a standard 
+#' nomenclature using \link[MungeSumstats]{standardise_header}.
+#' @param verbose Print messages.
+#' 
 #' @export
 #' @importFrom data.table data.table setnames
-mungesumstats_to_echolocatoR <- function(dat){
-    requireNamespace("data.table")
-    messager("+ Mapping colnames from MungeSumstats ==> echolocatoR")
+#' 
+#' @examples  
+#' dat <- dplyr::rename(echodata::BST1, BPOS=POS, SEBETA=StdErr, B=Effect)
+#' dat_echoR <- echodata::mungesumstats_to_echolocatoR(dat=dat, 
+#'                                                     standardise_colnames=TRUE)
+mungesumstats_to_echolocatoR <- function(dat, 
+                                         standardise_colnames = FALSE, 
+                                         verbose = TRUE){
+    
+    messager("+ Mapping colnames from MungeSumstats ==> echolocatoR",
+             v=verbose)
     dat <- data.table::data.table(dat)
+    if(isTRUE(standardise_colnames)){ 
+        dat <- MungeSumstats::standardise_header(
+            sumstats_dt = dat, 
+            uppercase_unmapped = TRUE,
+            return_list = FALSE)
+    } 
     colMap <- c("BP"="POS",
                 "FRQ"="Freq",
+                "FREQ"="Freq",
                 "BETA"="Effect",
+                "EFFECT"="Effect",
                 "SE"="StdErr",
+                "STDERR"="StdErr",
                 "N_CAS"="N_cases",
-                "N_CON"="N_controls")
+                "N_CON"="N_controls",
+                "SUPPORT"="Support",
+                "LEADSNP"="leadSNP",
+                "CONSENSUS_SNP"="Consensus_SNP",
+                "PROPORTION_CASES"="proportion_cases",
+                "MEAN.CS"=" mean.CS",
+                "MEAN.PP"=" mean.PP"
+                )
     colMap <- colMap[names(colMap) %in% colnames(dat)]
     for(x in names(colMap)){
         data.table::setnames(dat, x, colMap[[x]])
