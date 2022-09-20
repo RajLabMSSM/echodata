e07e647f455075c5908efeb235c38ee630c7cc3e
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-09-20 13:37:19 +0100

    Fix colmap unit test

--- tests/testthat/test-construct_colmap.R
+++ tests/testthat/test-construct_colmap.R
@@ -4,11 +4,11 @@ test_that("construct_colmap works", {
                 "P","Effect","StdErr","tstat",
                 "Locus","Freq","MAF","A1","A2",
                 "Gene","N_cases","N_controls",
-                "proportion_cases","N_col","verbose")
+                "proportion_cases","N","verbose")
     colmap <- construct_colmap(munged=TRUE)
     testthat::expect_true(all(fields %in% names(colmap)))
     
-    colmap <- construct_colmap(munged=TRUE, N_col = "N_cases")
+    colmap <- construct_colmap(munged=TRUE, N = "N_cases")
     testthat::expect_true(all(fields %in% names(colmap)))
-    testthat::expect_equal(colmap$N_col, "N")
+    testthat::expect_equal(colmap$N, "N")
 })
